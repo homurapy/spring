f4cb120907d2d70e09b71cb3101847e66525d6dd
Alex <2640178@gmail.com>
2021-04-29 16:28:50 +0300

    Задание 3

--- mvc/src/main/java/com/example/service/ProductService.java
+++ mvc/src/main/java/com/example/service/ProductService.java
@@ -3,7 +3,7 @@ package com.example.service;
 import com.example.dto.Product;
 import com.example.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,12 +11,12 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-//@Log4j
+@Log4j2
 public class ProductService {
     private final ProductRepository productRepository;
 
     public void add(Product product) {
-//        log.debug("Adding person");
+        log.debug("Adding product");
         if (product.getId() == null) {
             product.setId(UUID.randomUUID());
         }
@@ -24,11 +24,11 @@ public class ProductService {
     }
 
     public List<Product> findAll() {
-//        log.debug("findAll");
+        log.debug("findAll");
         return productRepository.findAll();
     }
     public Product findUno(UUID uuid) {
-//        log.debug("findUno");
+        log.debug("findUno");
         return productRepository.findUno(uuid);
     }
 }
